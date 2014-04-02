0f96812d76ddaf47792560bfd0641e3ecb3671f6
Contron <connorhaigh@gmail.com>
2014-04-02 17:19:12 +0100

    Removed debug code.

--- src/com/connorhaigh/javavpk/JavaVPK.java
+++ src/com/connorhaigh/javavpk/JavaVPK.java
@@ -1,26 +1,7 @@
 package com.connorhaigh.javavpk;
 
-import java.io.File;
-
-import com.connorhaigh.javavpk.core.Archive;
-
 public class JavaVPK 
 {
-	public static void main(String[] args)
-	{
-		File archiveFile = new File("D:/Program Files (x86)/Steam/steamapps/common/team fortress 2/tf/tf2_misc_dir.vpk");
-		Archive archive = new Archive(archiveFile);
-		
-		try
-		{
-			archive.load();
-		}
-		catch (Exception ex)
-		{
-			ex.printStackTrace();
-		}
-	}
-	
 	public static final int MINIMUM_VERSION = 2;
 	public static final int MAXIMUM_VERSION = 2;
 }
